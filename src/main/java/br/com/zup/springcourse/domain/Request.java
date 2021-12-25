6b948a35db9501aaceaf553dbc8f4787ed2023e7
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-12-25 19:27:03 -0300

    Use lombok's constructor annotations

--- src/main/java/br/com/zup/springcourse/domain/Request.java
+++ src/main/java/br/com/zup/springcourse/domain/Request.java
@@ -1,8 +1,15 @@
 package br.com.zup.springcourse.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class Request {
     private Long id;
