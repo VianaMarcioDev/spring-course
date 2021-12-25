d1e5c6120377f29304d5f934844b8efa2653c6bd
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-12-25 19:19:25 -0300

    Use annotation @Setter in User class

--- src/main/java/br/com/zup/springcourse/domain/User.java
+++ src/main/java/br/com/zup/springcourse/domain/User.java
@@ -1,11 +1,12 @@
 package br.com.zup.springcourse.domain;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Getter
+@Getter @Setter
 public class User {
 
     private Long id;
