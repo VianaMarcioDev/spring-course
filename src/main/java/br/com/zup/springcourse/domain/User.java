5025a7857919f3c06fbe55441b69b8f8f1bb5c1c
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-12-25 19:20:25 -0300

    Remove contructors from User class

--- src/main/java/br/com/zup/springcourse/domain/User.java
+++ src/main/java/br/com/zup/springcourse/domain/User.java
@@ -15,16 +15,6 @@ public class User {
     private String password;
     private List<Request> requests = new ArrayList<>();
 
-    public User(Long id, String name, String email, String password, List<Request> requests) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.password = password;
-        this.requests = requests;
-    }
-
-    public User() {
-    }
 
 
 }
