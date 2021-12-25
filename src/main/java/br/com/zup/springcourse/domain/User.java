35ab16d10d689fe6f7d58c0ca05ac814c668b485
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-12-25 19:18:05 -0300

    Create User class attributes

--- src/main/java/br/com/zup/springcourse/domain/User.java
+++ src/main/java/br/com/zup/springcourse/domain/User.java
@@ -1,4 +1,26 @@
 package br.com.zup.springcourse.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private List<Request> requests = new ArrayList<>();
+
+    public User(Long id, String name, String email, String password, List<Request> requests) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.requests = requests;
+    }
+
+    public User() {
+    }
+
+
 }
