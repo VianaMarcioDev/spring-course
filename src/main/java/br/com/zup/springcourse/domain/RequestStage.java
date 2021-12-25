2aac77fd5998247f28a17298a17db8dae086cadd
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-12-25 19:31:35 -0300

    Use annotation @Getter/@Setter in User class

--- src/main/java/br/com/zup/springcourse/domain/RequestStage.java
+++ src/main/java/br/com/zup/springcourse/domain/RequestStage.java
@@ -2,12 +2,16 @@ package br.com.zup.springcourse.domain;
 
 import br.com.zup.springcourse.domain.enums.RequestState;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Date;
 
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 public class RequestStage {
     private Long id;
     private String description;
