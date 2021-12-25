479b92b5c84f4d35badb28d5a337839e9627880a
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-12-25 19:38:34 -0300

    Create Role attribute in User class

--- src/main/java/br/com/zup/springcourse/domain/User.java
+++ src/main/java/br/com/zup/springcourse/domain/User.java
@@ -1,5 +1,6 @@
 package br.com.zup.springcourse.domain;
 
+import br.com.zup.springcourse.domain.enums.Role;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,6 +18,7 @@ public class User {
     private String name;
     private String email;
     private String password;
+    private Role role;
     private List<Request> requests = new ArrayList<>();
     private List<RequestStage> stages = new ArrayList<>();
 
