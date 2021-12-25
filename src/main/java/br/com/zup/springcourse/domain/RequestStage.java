71462f0ae098689aa49452234e9f2f08704a08f9
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-12-25 19:31:17 -0300

    Use lombok's constructor annotations

--- src/main/java/br/com/zup/springcourse/domain/RequestStage.java
+++ src/main/java/br/com/zup/springcourse/domain/RequestStage.java
@@ -1,9 +1,13 @@
 package br.com.zup.springcourse.domain;
 
 import br.com.zup.springcourse.domain.enums.RequestState;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
+@NoArgsConstructor
+@AllArgsConstructor
 public class RequestStage {
     private Long id;
     private String description;
