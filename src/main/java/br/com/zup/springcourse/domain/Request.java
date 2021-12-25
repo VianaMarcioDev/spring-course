2886974248d429b8afdeeb9fc7a20e959215804a
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-12-25 19:26:32 -0300

    Create attributes to Request class

--- src/main/java/br/com/zup/springcourse/domain/Request.java
+++ src/main/java/br/com/zup/springcourse/domain/Request.java
@@ -1,4 +1,15 @@
 package br.com.zup.springcourse.domain;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class Request {
+    private Long id;
+    private String subject;
+    private String description;
+    private Date creationDate;
+    private RequestState state;
+    private User user;
+    private List<RequestState> states = new ArrayList<>();
 }
